1d515a84a28e000f7df04b1c00be66d586c3c338
Abstract02 <adarsh526222@gmail.com>
2022-07-22 00:23:40 +0530

    removed firebase files

--- src/Login.js
+++ src/Login.js
@@ -1,41 +1,16 @@
 import React, { useState } from 'react';
 import './Login.css'
 import { Link, useNavigate } from "react-router-dom";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 import { create } from '@mui/material/styles/createTransitions';
-import app from './firebase';
 
 function Login() {
-    const authentication = getAuth();
+    
     let navigate = useNavigate();
     
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const Register =()=>
-    {
-        
-        createUserWithEmailAndPassword(authentication,email,password)
-        .then(()=>{
-            alert("User Created Sucessfully")
-            navigate('/')
-        })
-        .catch(()=>{
-            alert("Email alredy in use")
-        })
-    }    
-    const Login =()=>
-    {
-        console.log("auth")
-        // signInWithEmailAndPassword(authentication,email,password)
-        // .then(()=>{
-        //     alert("User Created Sucessfully")
-        //     navigate('/');
-        // })
-        // .catch((err)=>{
-        //     console.log(err)
-        // })
-    }    
+ 
 
     return (
         <div className='login'>
@@ -56,7 +31,7 @@ function Login() {
                     <h5>Password</h5>
                     <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
 
-                    <button  className='Login_signin_Button' onClick={Login} >Sign In</button>
+                    <button  className='Login_signin_Button' >Sign In</button>
                 </form>
 
                 <p>
@@ -64,7 +39,7 @@ function Login() {
                     see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
                 </p>
 
-                <button className='register_button' onClick={Register}>Create your Amazon Account</button>
+                <button className='register_button' >Create your Amazon Account</button>
             </div>
         </div>
     )
